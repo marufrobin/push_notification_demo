21c3278681d801352e65b5f7af52a7d73541016c
Maruf Robin <maruf.robin@northsouth.edu>
2022-12-17 00:34:43 +0600

    local notification works but push notification doesn't

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,42 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() async {
+const AndroidNotificationChannel androidNotificationChannel =
+    AndroidNotificationChannel(
+  'this is the ID',
+  "this is the title",
+  description: "this is the Notification desctiption",
+  importance: Importance.high,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("A BackGround Message just showed UP::${message.messageId}");
+  print("BackGround Message ::${message}");
+}
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(androidNotificationChannel);
+
+  FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
   runApp(const MyApp());
 }
 
@@ -35,10 +68,73 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? androidNotification = message.notification?.android;
+
+      if (notification == null && androidNotification == null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification?.title,
+          notification?.body,
+          NotificationDetails(
+              android: AndroidNotificationDetails(
+            androidNotificationChannel.id,
+            androidNotificationChannel.name,
+            channelDescription: androidNotificationChannel.description,
+            color: Colors.blue,
+            playSound: true,
+            icon: "@mipmap/ic_launcher",
+          )),
+        );
+      }
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("A new openMessageAPP event publisheed");
+      RemoteNotification? remoteNotification = message.notification;
+      AndroidNotification? androidNotification = message.notification?.android;
+
+      if (remoteNotification == null && androidNotification == null) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("${remoteNotification?.title}"),
+              content: Column(
+                children: [
+                  Text("${remoteNotification!.body}"),
+                ],
+              ),
+            );
+          },
+        );
+      }
+    });
+    // click notification we will show in alert
+  }
+
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+    flutterLocalNotificationsPlugin.show(
+      0,
+      "Testing$_counter",
+      "Testing the notification first time",
+      NotificationDetails(
+          android: AndroidNotificationDetails(
+        androidNotificationChannel.id,
+        androidNotificationChannel.name,
+        channelDescription: androidNotificationChannel.description,
+        color: Colors.blue,
+        playSound: true,
+        icon: "@mipmap/ic_launcher",
+      )),
+    );
   }
 
   @override
